d2bfa19bfb47ac194dc6cd4116cd44eeda82f581
MrChike <chikeegonu@gmail.com>
2019-11-09 15:12:21 +0100

    cleared error

--- budgetProject/budget/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Expense, Category
-# Register your models here.
-
-admin.site.register(Project)
-admin.site.register(Expense)
-admin.site.register(Category)
