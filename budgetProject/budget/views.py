e5f112768597cd88fa2e90e86cf46b80ed8cfe89
MrChike <chikeegonu@gmail.com>
2019-11-09 14:55:54 +0100

    update

--- budgetProject/budget/views.py
+++ budgetProject/budget/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 # Create your views here.
 
 
@@ -8,4 +8,6 @@ def project_list(request):
 
 
 def project_detail(request, project_slug):
-    return render(request, 'budget/project-detail.html')
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'budget/project-detail.html',
+                  {'project': project, 'expense_list': project.expenses.all()})
